30991d5ca08dbab7decd5c2da06034123df5b94f
ParkJong-Hun <thesn515@gmail.com>
2021-08-27 09:52:25 +0900

    2021/08/27 09:52 First Study

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+#Controller 만드는 법 : rails generate controller <컨트롤러 이름>
+class HomeController < ApplicationController
+    def index
+
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  #get '/' => 'home#index' : /로 접속을하면 home controller 안에 있는 index로 작업을 하라고 함.
+  get '/index' => 'home#index'
 end
