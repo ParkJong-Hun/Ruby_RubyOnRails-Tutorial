5918885ab4f34a22445f7db06a3b607ceb86f5cc
ParkJong-Hun <thesn515@gmail.com>
2021-08-27 14:26:42 +0900

    2021/08/27 14:26 Use Variable

---
+++ app/controllers/calculator_controller.rb
@@ -0,0 +1,6 @@
+class CalculatorController < ApplicationController
+    #add라는 action 만들기
+    def add 
+        @message = "Hello I'm information"
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   #get '/' => 'home#index' : /로 접속을하면 home controller 안에 있는 index로 작업을 하라고 함.
+  #action과 url을 연결하기
   get '/index' => 'home#index'
+  get '/add' => 'calculator#add'
 end
