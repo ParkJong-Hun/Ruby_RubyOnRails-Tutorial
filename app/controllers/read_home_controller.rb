c862c99f147620d08339715da79021d6ffa3ad13
ParkJong-Hun <thesn515@gmail.com>
2021-08-29 09:17:17 +0900

    2021/08/29 09:17 Use Update, Delete

--- app/controllers/read_home_controller.rb
+++ app/controllers/read_home_controller.rb
@@ -17,4 +17,25 @@ class ReadHomeController < ApplicationController
         #다른 페이지로 이동시키기
         redirect_to '/read/index'
     end
+
+    def modify
+        #Post.all과 다르게 하나만 가져옴
+        @post = Post.find(params[:post_id])
+    end
+
+    def update
+        #한 데이터의 정보를 수정하여 업데이트하는 기본 명령어
+        post = Post.find(params[:post_id])
+        post.title = params[:title]
+        post.content = params[:content]
+        post.save
+        redirect_to '/read/index'
+    end
+
+    def delete
+        #한 데이터를 삭제하기
+        Post.destroy(params[:post_id])
+        #뒤로 가기 redirect_back(구 버전: redirect_to :back)
+        redirect_to '/read/index'
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,16 @@ Rails.application.routes.draw do
   #action과 url을 연결하기
   get '/index' => 'home#index'
   get '/add' => 'calculator#add'
+
   post '/result' => 'calculator#result'
   #input name을 가져와서 사용할 수 있다.
   get '/result/:first/:second' => 'calculator#result'
+
   get '/read/index' => 'read_home#index'
   get '/read/write' => 'read_home#write'
   post '/read/create' => 'read_home#create'
+  #:으로 모델의 데이터 정보를 받아오기
+  get '/read/modify/:post_id' => 'read_home#modify'
+  post '/read/update/:post_id' => 'read_home#update'
+  get '/read/delete/:post_id' => 'read_home#delete'
 end
